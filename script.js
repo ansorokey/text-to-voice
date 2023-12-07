49b0c12f57fd9fedda7e7ccce1dbba72b6e82fcc
ansorokey <ansorokey@gmail.com>
2023-12-07 10:35:13 -0500

    added all avaialble voices

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector('select');
+
+// provides all available voices
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+    voices.forEach((v, i) => {
+        voiceSelect.options[i] = new Option(v.name, i)
+    })
+}
 
 document.querySelector('button').addEventListener('click', () => {
     speech.text = document.querySelector('textarea').value;
